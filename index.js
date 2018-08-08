807131aed3ad3aedbeee814dff246108abf76347
Alena <i.am.am.629@gmail.com>
2018-08-08 17:42:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -138,10 +138,11 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   e.preventDefault()
    if(e.which == LEFT_ARROW){
+     e.preventDefault()
      moveDodgerLeft()
    } else if (e.which == RIGHT_ARROW){
+     e.preventDefault()
      moveDodgerRight()
    }
 }
