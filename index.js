b67d59bafd7f697e8503e175bf5044399a8a12a6
Alena <i.am.am.629@gmail.com>
2018-08-08 17:25:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,9 +46,9 @@ function checkCollision(rock) {
          *    and the rock's right edge is > the DODGER's right edge
          */
 
-    return (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge)
-    || (rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge)
-    || (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)
+    return (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge)
+    || (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge)
+    || (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
   }
 }
 
